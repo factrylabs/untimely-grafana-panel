51a8c157343c174cbdacb036f31e06ccc4badb9c
Pieterjan Lambrecht <me@pielambr.be>
2020-07-23 14:23:04 +0200

    Use Input component and show extra help

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react';
-import { FormField, Select } from '@grafana/ui';
+import {
+  InfoBox, Field, Input, Select,
+} from '@grafana/ui';
 import { PanelEditorProps } from '@grafana/data';
 
 import { SimpleOptions } from './types';
@@ -32,45 +34,58 @@ export class Editor extends PureComponent<PanelEditorProps<SimpleOptions>> {
 
     const selectedXSeries = selectSeriesOptions.find((serie) => serie.value === xseries);
 
+    const ySeries = data.series.filter((s) => !!xseries && s.name !== xseries);
+
     return (
       <>
-        <div className="gf-form-group">
-          <h5 className="section-heading">Formatting options</h5>
-          <FormField
-            label="Decimals after comma"
-            labelWidth={20}
-            inputWidth={20}
+        <Field
+          label="Decimals after comma"
+          description=""
+        >
+          <Input
+            css=""
+            onChange={({ currentTarget: { value } }) => (
+              this.onChange('accuracy', value)
+            )}
             type="number"
             value={accuracy}
-            onChange={({ target: { value } }) => this.onChange('accuracy', value)}
           />
-        </div>
-        <div className="gf-form-group">
-          <h5 className="section-heading">X-Axis series</h5>
+        </Field>
+        <Field
+          label="Reference series"
+        >
           <Select
             placeholder="Choose..."
             options={selectSeriesOptions}
             value={selectedXSeries}
             onChange={({ value }) => this.onChange('xseries', value)}
           />
-        </div>
-        {!!xseries && (
-          <div className="gf-form-group">
-            <h1 className="section-heading">Y-series offsets</h1>
-            {data.series.filter((serie) => serie.name !== xseries).map((serie) => (
-              <FormField
-                label={serie.name || ''}
-                labelWidth={20}
-                inputWidth={20}
-                type="number"
-                value={offsets[serie.name as string]}
-                onChange={({ target: { value } }) => (
-                  this.onChangeOffset(serie.name as string, value)
-                )}
-              />
-            ))}
-          </div>
+        </Field>
+        {!!xseries && !ySeries.length && (
+          <InfoBox
+            title="Add more than one series"
+          >
+            <p>
+              Please add more than one series to get started.
+              <br />
+              All series that are not the reference series will be used as Y-series.
+            </p>
+          </InfoBox>
         )}
+        {ySeries.map((serie) => (
+          <Field
+            label={`Offset for ${serie.name || ''}`}
+          >
+            <Input
+              css=""
+              type="number"
+              onChange={({ currentTarget: { value } }) => (
+                this.onChangeOffset(serie.name as string, value)
+              )}
+              value={offsets[serie.name as string]}
+            />
+          </Field>
+        ))}
       </>
     );
   }
