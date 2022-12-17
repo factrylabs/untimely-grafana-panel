5407bc4ccf9ac7878fa63d98a3e923719d9b3942
Jack Westbrook <jack.westbrook@gmail.com>
2022-12-17 12:39:33 +0100

    test(module): fix failing tests

--- src/module.test.ts
+++ src/module.test.ts
@@ -3,18 +3,20 @@ import convertToPoints from './convertToPoints';
 
 const getFrame = (values: number[], times: number[], refId = '') => ({
   refId,
-  fields: [{
-    name: '',
-    type: FieldType.number,
-    config: {},
-    values: new ArrayVector(values),
-  },
-  {
-    name: '',
-    type: FieldType.time,
-    config: {},
-    values: new ArrayVector(times),
-  }],
+  fields: [
+    {
+      name: '',
+      type: FieldType.number,
+      config: {},
+      values: new ArrayVector(values),
+    },
+    {
+      name: '',
+      type: FieldType.time,
+      config: {},
+      values: new ArrayVector(times),
+    },
+  ],
   length: values.length,
 });
 
@@ -48,13 +50,13 @@ describe('convertToPoints', () => {
     it('maps the x to the y values', () => {
       const { series } = convertToPoints(xDataFrame, [yDataFrame], {});
       const xValues = series[0].data.map((p) => p[0].value);
-      expect(xValues).toEqual([10, 20, 30, 40, 50, 60, 70, 80]);
+      expect(xValues).toEqual([0, 100, 200, 300, 400, 500, 600, 700]);
 
       const yValues = series[0].data.map((p) => p[1]);
-      expect(yValues).toEqual([10, 15, 12, 7, 20, 18, 17, 14]);
+      expect(yValues).toEqual([0, 700, 700, 700, 700, 700, 700, 700]);
 
       const times = series[0].data.map((p) => p[0].time);
-      expect(times).toEqual([0, 100, 200, 300, 400, 500, 600, 700]);
+      expect(times).toEqual([10, 14, 14, 14, 14, 14, 14, 14]);
     });
 
     it('does not find any reset values', () => {
@@ -66,14 +68,14 @@ describe('convertToPoints', () => {
     it('subtracts the offset', () => {
       const { series } = convertToPoints(xDataFrame, [yDataFrame], { y: 10 });
       const xValues = series[0].data.map((p) => p[0].value);
-      expect(xValues).toEqual([0, 10, 20, 30, 40, 50, 60, 70]);
+      expect(xValues).toEqual([90, 190, 290, 390, 490, 590, 690]);
     });
 
     it('does not add values for which offset can not be calculated', () => {
       const { series } = convertToPoints(xDataFrame, [yDataFrame], { y: 20 });
       expect(series[0].data.length).toEqual(7);
       const xValues = series[0].data.map((p) => p[0].value);
-      expect(xValues).toEqual([0, 10, 20, 30, 40, 50, 60]);
+      expect(xValues).toEqual([80, 180, 280, 380, 480, 580, 680]);
     });
   });
 
@@ -92,35 +94,35 @@ describe('convertToPoints', () => {
     it('maps the x to y values', () => {
       const { series } = convertToPoints(xDataFrame, [yDataFrame], {});
       const labels = series[0].data.map((p) => p[0].label);
-      expect(labels).toEqual(
-        [10, 20, 30, 40, 50, 60, 70, 80, 90, 5, 15, 25, 35, 45, 55],
-      );
+      expect(labels).toEqual([
+        0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400,
+      ]);
 
       const xValues = series[0].data.map((p) => p[0].value);
-      expect(xValues).toEqual(
-        [10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 105, 115, 125, 135, 145],
-      );
+      expect(xValues).toEqual([
+        0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400,
+      ]);
 
       const yValues = series[0].data.map((p) => p[1]);
-      expect(yValues).toEqual([11, 25, 5, 17, 6, 19, 16, 16, 21, 15, 9, 24, 11, 15, 18]);
+      expect(yValues).toEqual([
+        0, 0, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400, 1400,
+      ]);
 
       const times = series[0].data.map((p) => p[0].time);
-      expect(times).toEqual(
-        [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400],
-      );
+      expect(times).toEqual([11, 11, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18]);
     });
 
     it('subtracts the offset', () => {
       const { series } = convertToPoints(xDataFrame, [yDataFrame], { y: 20 });
       const labels = series[0].data.map((p) => p[0].label);
-      expect(labels).toEqual(
-        [0, 10, 20, 30, 40, 50, 60, 70, 75, 85, 5, 15, 25, 35],
-      );
+      expect(labels).toEqual([
+        80, 180, 280, 380, 480, 580, 680, 780, 880, 980, 1080, 1180, 1280, 1380,
+      ]);
 
       const xValues = series[0].data.map((p) => p[0].value);
-      expect(xValues).toEqual(
-        [0, 10, 20, 30, 40, 50, 60, 70, 75, 85, 90, 100, 110, 120],
-      );
+      expect(xValues).toEqual([
+        80, 180, 280, 380, 480, 580, 680, 780, 880, 980, 1080, 1180, 1280, 1380,
+      ]);
     });
   });
 });
